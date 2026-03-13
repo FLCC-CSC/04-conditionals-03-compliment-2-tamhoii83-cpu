cf551ba3144b16e5281bc76e4035ba6a4e413d48
tamhoii83-cpu <tamhoii83@gmail.com>
2026-03-13 03:10:41 +0000

    wrote my solution for compliment_02

--- compliment_02.py
+++ compliment_02.py
@@ -1,80 +1,23 @@
 # FILE NAME - compliment_02.py
 
-# NAME: 
-# DATE: 
-# BRIEF DESCRIPTION:  
-
-
-
-# 1. Make sure you fill out the comments above
-# 2. Write your code in the proper spot
-# 3. Be sure to answer the Reflection Questions and Attestation below
-# 4. The Sample Output has been included in this code for your convenience
-
-
+# NAME: Hoi I Tam
+# DATE: March 12, 2026
+# BRIEF DESCRIPTION:  The user will enter yes to have a compliment. Any other input will result in a No compliment for you! message.
 
 ########## ENTER YER CODE BELOW THIS LINE ##########
 
+answer = input("Would you like a compliment?")
+if answer == "yes":
+    print("You have wonderful eyes.")
+else:
+    print("No compliment for you!")
 
-
-
-
-
-
-
-
+print("Thank you for playing.")
 
 ########### END YER CODE ABOVE THIS LINE ###########
-
-    
-
-
-
-########################################
-#          SAMPLE OUTPUT
-########################################
-
-'''
-Would you like a compliment? yes
-You have wonderful eyes.
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? Yes
-No compliment for you!
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? y
-No compliment for you!
-Thank you for playing.
-'''
-
-
-'''
-Would you like a compliment? no
-No compliment for you!
-Thank you for playing.
-'''
-
-
-
-########################################
-#          REFLECTION QUESTIONS
-########################################
-
 '''
 
 1. Did you struggle with this lab (YES/NO)?
-
-
-
-
-
-
+NO
 
 '''
